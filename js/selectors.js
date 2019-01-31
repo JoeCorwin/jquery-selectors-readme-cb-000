7935e251213c815049902eb805e94d81958202c5
Joe Corwin <joe.corwin@gmail.com>
2019-01-31 01:41:53 +0000

    Automatically backed up by Learn

--- js/selectors.js
+++ js/selectors.js
@@ -11,9 +11,11 @@ function ninjaBabySelector() {
   return $('#baby-ninja')
 }
 
+function divSelector() {
+  return $('div')
 
-// Write a function ninjaBabySelector that does not accept any parameters. The function should use an ID selector to return the ninja baby image.
-//
+
+}
 // Write a function divSelector that does not accept any parameters. The function should use a class selector to return the two divs with the class pics.
 //
 // Write a function firstListItem that does not accept any parameters. The function should use a first-child selector to return the first list item in the ul with the ID pic-list
